77684cf210de40f80fde34b21f4b0873a3af81a2
Joel Bartlett <joel@murbar.com>
2019-03-28 16:39:17 -0400

    Add pragma (enforce FK)

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,12 @@ module.exports = {
     },
     seeds: {
       directory: './data/migrations'
+    },
+    // by default SQLite will not enforce foreign keys
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done); // enforce FK
+      }
     }
   }
 };
