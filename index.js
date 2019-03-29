b0863f6c47ee0e97ba7aa76c00ecb0e0363e67df
Joel Bartlett <joel@murbar.com>
2019-03-28 21:51:42 -0400

    Test functions

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const db = require('./data/db');
 
 const tests = async () => {
-  const dishes = await db.getDishes();
-  console.log(dishes);
+  // const dishes = await db.getDishes();
+  // console.log(dishes);
+  // const newDish = await db.addDish({
+  //   name: 'Chocolate Chip Cookies 5',
+  //   cuisine: 'American'
+  // });
+  // console.log(newDish);
+  // const dish = await db.getDish(9);
+  // console.log(dish);
+  // const recipes = await db.getRecipes();
+  // console.log(recipes);
+  // const newRecipe = await db.addRecipe({
+  //   name: 'Some recipe',
+  //   method: 'Lorem ispum dolor.',
+  //   dish_id: 11
+  // });
+  // console.log(newRecipe);
 };
 
 tests();
